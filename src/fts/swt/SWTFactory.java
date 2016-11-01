406fc18d4099b852e296a3d944c421a3c9b01862
Franco Catrin <fcatrin@gmail.com>
2016-11-01 14:58:34 -0300

    change API

--- src/fts/swt/SWTFactory.java
+++ src/fts/swt/SWTFactory.java
@@ -1,10 +1,18 @@
 package fts.swt;
 
+import org.w3c.dom.Node;
+
 import fts.core.ComponentFactory;
 import fts.core.Window;
+import fts.views.View;
 
 public class SWTFactory implements ComponentFactory {
 	public Window createWindow() {
 		return new SWTWindow();
+	}
+
+	@Override
+	public View createView(Node node) {
+		return null;
 	}
 }
